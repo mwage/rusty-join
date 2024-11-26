8717075d66757510d0f96df131f6d6e5c6a60afc
EP2024 12120487 <ep12120487@g0.complang.tuwien.ac.at>
2024-11-26 11:24:31 +0100

    split_no_encode_pattern_matching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::env;
 
-use rusty_join::split_no_encode;
+//use rusty_join::split_no_encode;
+use rusty_join::split_no_encode_pattern_matching;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // all_hash(args);          // ~136m
     // quintuple_sort(args);    // ~129m
     // split_during_read(args);    // ~133/138m
-    split_no_encode(args); // ~100m
+    //split_no_encode(args); // ~100m/~84m
+    split_no_encode_pattern_matching(args); //~95m/~84m
 }
 /// #DON"T forget to use your implementation above!!!"
 /// Tests the functionality of the current solution defined above (comment out the ones youdont want)
---
+++ src/split_no_encode_pattern_matching.rs
@@ -0,0 +1,31 @@
+use crate::helper::*;
+use rustc_hash::FxHashMap;
+
+// Reads all files into hash maps with the first column as key, then merges together accordingly.
+// Values are not encoded, just stored as a string.
+pub fn split_no_encode_pattern_matching(args: Vec<String>) {
+    let (f1, f2, f3, f4) = (
+        read_file_split_no_encoding(&args[1]), read_file_split_no_encoding(&args[2]),
+        read_file_split_no_encoding(&args[3]), read_file_split_no_encoding(&args[4])
+    );
+    join_first_three_and_output_with_forth(f1, f2, f3, f4);
+}
+
+// Iterates the hashmaps and outputs all correct matches
+fn join_first_three_and_output_with_forth(f1: FxHashMap<String, Vec<String>>, f2: FxHashMap<String, Vec<String>>, f3: FxHashMap<String, Vec<String>>, f4: FxHashMap<String, Vec<String>>) {
+    for (key, vec1) in f1.iter() {
+        if let (Some(vec2), Some(vec3)) = (f2.get(key), f3.get(key)) {
+            for x1 in vec1 {
+                for x2 in vec2 {
+                    for x3 in vec3 {
+                        if let Some(vec4) = f4.get(x3) {
+                            for x4 in vec4 {
+                                println!("{},{},{},{},{}", x3, key, x1, x2, x4);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
