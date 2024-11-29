7daab2ac5b312d051c95d2d82643825972b9bf54
Michael Wagner <michael.joh.wagner@gmail.com>
2024-11-29 22:14:08 +0100

    compact string (great) + parse to &str (bad)

--- src/helper.rs
+++ src/helper.rs
@@ -3,6 +3,7 @@ use rustc_hash::FxHashMap;
 use typenum::U2;
 use std::fs::read_to_string;
 use crate::encoder::Encoder;
+use compact_str::CompactString;
 
 // Sorts file by key position
 pub fn sort<F: ArrayLength>(file: &mut Vec<GenericArray<usize, F>>, pos: usize) {
@@ -37,6 +38,18 @@ pub fn read_file_split_no_encoding(file: &String) -> FxHashMap<String, Vec<Strin
 
     for line in read_to_string(file).unwrap().lines() {
         let mut split = line.split(",").map(|x| x.to_string());
+        map.entry(split.next().unwrap()).or_default().push(split.next().unwrap());
+    }
+
+    map
+}
+
+
+pub fn read_file_split_no_encoding_compact(file: &String) -> FxHashMap<CompactString, Vec<CompactString>> {
+    let mut map: FxHashMap<CompactString, Vec<CompactString>> = FxHashMap::default();
+
+    for line in read_to_string(file).unwrap().lines() {
+        let mut split = line.split(",").map(|x| CompactString::from(x));
         map.entry(split.next().unwrap()).or_default().push(split.next().unwrap());
     }
 
