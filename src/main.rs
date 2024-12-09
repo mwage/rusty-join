a55cf7970feae8603bd05795c2ec8e124e86e72f
EP2024 12120487 <ep12120487@g0.complang.tuwien.ac.at>
2024-12-09 13:04:31 +0100

    SmallVec impl

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ use rusty_join::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    all_hash(args);
+    //all_hash(args);
     // quintuple_sort(args);
     // split_during_read(args);
     //split_no_encode(args);
-    // split_no_encode_pattern_matching(args);
+    split_no_encode_pattern_matching(args);
 }
 
 /// #DON"T forget to use your implementation above!!!"
